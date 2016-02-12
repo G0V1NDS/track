dc3e0438f92f28da728d73e28f6c13423140b121
Govind Singh Rajpurohit <Govind Singh Rajpurohit>
2016-02-12 13:48:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+	io.set('log level', 1);
+var mover=new Array(),monitor=new Array();
+server.listen(4000);
+app.use(express.static('files'));
+
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/index.html');
+});
+
+//io.on and io.sockets.on are same
+	io.on('connection', function(client){
+					console.log('User Connected');
+					client.on('submit',function(data){
+						var obj;
+						obj={'name':data.name,'socket':client};
+						if(data.mode==='Move')
+						{
+							mover.push(obj);
+							console.log('new user added');
+							client.broadcast.emit('new marker',data.marker_id);
+						}
+						else
+						{
+							monitor.push(obj);
+						}
+					});
+
+					 client.on('new position',function(data){
+					 	client.broadcast.emit('show move',data);
+					 });
+
+
+					 client.on('disconnect', function(){
+						 remove_marker(client,mover,monitor);
+	 				});
+});
+function remove_marker(client,mover,monitor){
+	console.log('disconnect event fired');
+	io.sockets.emit('test','Hello dear');
+		 /*var obj;
+		 for(i=0;i<mover.length;i++)
+		 {
+			 if(mover[i].socket===client)
+			 {
+				 console.log('mover removed');
+				 obj=mover[i];
+				 mover.splice(i,1);
+				 break;
+			 }
+		 }
+		 for(i=0;i<monitor.length;i++)
+		 {
+			 if(monitor[i].socket===client)
+			 {
+				 console.log('monitor removed');
+				 obj=monitor[i];
+				 monitor.splice(i,1);
+				 break;
+			 }
+		 }
+
+		 console.log(obj.name);
+		 client.emit('remove',obj.name);
+		 console.log(obj.name);*/
+}
