f72f80b042a786369b334c31e7d662e8994ca7da
Govind Singh Rajpurohit <Govind Singh Rajpurohit>
2016-02-13 02:19:22 +0530

    Mover and monitor concept removed, simple interface

--- index.js
+++ index.js
@@ -3,65 +3,62 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 	//io.set('log level', 1);
-var mover=new Array(),monitor=new Array();
+
 server.listen(process.env.PORT || 5000);
 app.use(express.static('files'));
 
 app.get('/', function(req, res){
-	res.sendfile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
+var usr=new Array();
+var position=new Array();
+var id=new Array();
 //io.on and io.sockets.on are same
 	io.on('connection', function(client){
-					console.log('User Connected');
 					client.on('submit',function(data){
-						var obj;
-						obj={'name':data.name,'socket':client};
-						if(data.mode==='Move')
-						{
-							mover.push(obj);
-							console.log('new user added');
-							client.broadcast.emit('new marker',data.marker_id);
-						}
-						else
-						{
-							monitor.push(obj);
-						}
+						var obj={'name':data.name,'socket':client,'coords':data.coords};
+						console.log('New user online');
+						usr.push(obj);
+						position.push(obj.coords);
+						id.push(obj.name);
+						io.emit('give info',{'position':position,'id':id});
 					});
 
-					 client.on('new position',function(data){
-					 	client.broadcast.emit('show move',data);
-					 });
+					client.on('new position',function(data){
+						for(i=0;i<id.length;i++)
+						{
+							if(id[i]===data.id)
+							{
+								position[i]=data.coords;
+								break;
+							}
+						}
+					 	io.emit('show move',data);
+					});
 
 
-					 client.on('disconnect', function(){
-						 remove_marker(client,mover,monitor);
+					client.on('disconnect', function(){
+						 remove_position(client,usr);
 	 				});
 });
-function remove_marker(client,mover,monitor){
-	console.log('disconnect event fired');
+function remove_position(client,usr){
 		 var obj;
-		 for(i=0;i<mover.length;i++)
+		 for(i=0;i<usr.length;i++)
 		 {
-			 if(mover[i].socket===client)
+			 if(usr[i].socket===client)
 			 {
-				 console.log('mover removed');
-				 obj=mover[i];
-				 mover.splice(i,1);
+				 console.log('User is going offline');
+				 obj=usr[i];
+				 position.splice(i,1);
+				 id.splice(i,1);
+				 usr.splice(i,1);
 				 break;
 			 }
 		 }
-		 for(i=0;i<monitor.length;i++)
-		 {
-			 if(monitor[i].socket===client)
-			 {
-				 console.log('monitor removed');
-				 obj=monitor[i];
-				 monitor.splice(i,1);
-				 break;
-			 }
-		 }
-
+		if(obj)
+		{
 		 io.emit('remove',obj.name);
 		 console.log(obj.name);		 
+		}
 }
